3fc0b2a866984f5d70384ffbe22f1ce7946fae54
Richard Nii Lante Lawson <niilante@users.noreply.github.com>
2018-01-21 22:23:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ webhook_url = process.env.APP_URL || c.HEROKU_URL;
 var validate_network_type = (c.TESTNET ? 'testnet':'prod');
 
 var sql_config = process.env.DATABASE_URL || {
-  user: 'postgres',
-  database: 'postgres',
-  password: 'password',
-  host: 'localhost',
+  user: 'nshvfjntrdxvln',
+  database: 'd8pms2q2kvn6bn',
+  password: '8e038ef7eb1f32a18f112b47f7d2d5c1affdcafdaef5133089011c5be9caca4f',
+  host: 'ec2-54-221-198-206.compute-1.amazonaws.com',
   port: 5432,
 };
 
