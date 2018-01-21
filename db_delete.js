21e3c8d8ca2571d840a79b67ffe5e58ff36f9d8d
Richard Nii Lante Lawson <niilante@users.noreply.github.com>
2018-01-21 22:21:20 +0000

    Update db_delete.js

--- db_delete.js
+++ db_delete.js
@@ -3,10 +3,10 @@ var pg = require('pg')
     , query;
 
 var config = process.env.DATABASE_URL || {
-  user: 'postgres', 
-  database: 'postgres',
-  password: 'password',
-  host: 'localhost',
+  user: 'nshvfjntrdxvln', 
+  database: 'd8pms2q2kvn6bn',
+  password: '8e038ef7eb1f32a18f112b47f7d2d5c1affdcafdaef5133089011c5be9caca4f',
+  host: 'ec2-54-221-198-206.compute-1.amazonaws.com',
   port: 5432, 
 };
 
