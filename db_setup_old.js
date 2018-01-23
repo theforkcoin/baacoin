a26e10f81a278fc1b64e5e72c8663d325364037e
Richard Nii Lante Lawson <richardlawson97@gmail.com>
2018-01-23 21:35:33 +0000

    new update

--- db_setup_old.js
+++ db_setup_old.js
@@ -3,11 +3,11 @@ var pg = require('pg')
     , query;
 
 var config = process.env.DATABASE_URL || {
-  user: 'nshvfjntrdxvln', 
-  database: 'd8pms2q2kvn6bn', 
-  password: '8e038ef7eb1f32a18f112b47f7d2d5c1affdcafdaef5133089011c5be9caca4f', 
-  host: 'ec2-54-221-198-206.compute-1.amazonaws.com', 
-  port: 5432, 
+    user: 'postgres', 
+    database: 'postgres', 
+    password: 'password', 
+    host: 'localhost', 
+    port: 5432, 
 };
 
 client = new pg.Client(config);
